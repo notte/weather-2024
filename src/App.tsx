9a577479b76f769acb06c4de3b48f617d18a8537
yoyo <maonome@gmail.com>
2023-11-17 05:29:17 +0800

    add mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Routes, Route } from 'react-router-dom'
+import { useRef, useEffect, useState } from 'react'
+import mapboxgl, { Map } from 'mapbox-gl'
+
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoieW95bzIwMjMiLCJhIjoiY2xvd2dnNWR0MDR5dDJxcGl3cjAwczUwbiJ9.HPB6vtrEbhCQILNbIDqktA'
 
 const App = () => {
-  return <></>
+  const mapContainer = useRef<HTMLDivElement | null>(null)
+  const map = useRef<null | Map>(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return // 僅初始化地圖一次
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current as HTMLElement,
+      style: 'mapbox://styles/yoyo2023/clp0uor9i00c601r6cyl8g78b',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
+
+  return (
+    <>
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+    </>
+  )
 }
 
 export default App
